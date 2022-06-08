5b752dfafd02e4072877918b09ef5089cae26e16
Ben Yong <byong@deloitte.com.au>
2022-06-08 16:35:17 +1000

    [WIP] Initial skeleton of markdown templater

---
+++ markdown_templater.js
@@ -0,0 +1,9 @@
+function changeContent1() {
+    document.getElementById("show1").classList.remove("hide");
+    document.getElementById("show2").classList.add("hide");
+}
+  
+function changeContent2() {
+    document.getElementById("show1").classList.add("hide");
+    document.getElementById("show2").classList.remove("hide");
+}
